b611f050a7de1311f2ccd8cdfb55b8d5ea1bbc7a
Joaquim Cardeira <asklocation.net@gmail.com>
2025-04-25 01:38:23 +0000

    log

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ mkdir -p data
 # Uncomment or customize as needed
 # echo "Downloading .pbf files..."
 #wget -q -nc -O data/brazil.pbf https://download.geofabrik.de/south-america/brazil-latest.osm.pbf
-wget -q -nc -O data/chile.pbf https://download.geofabrik.de/south-america/chile-latest.osm.pbf
+wget -nc -O data/chile.pbf https://download.geofabrik.de/south-america/chile-latest.osm.pbf
 wget -q -nc -O data/portugal.pbf https://download.geofabrik.de/europe/portugal-latest.osm.pbf
 
 echo "Looking for .pbf files in ./data..."
@@ -25,7 +25,7 @@ if [ -z "$PBF_FILES" ]; then
   exit 1
 fi
 
-echo "Merging PBF files..."
+echo "Merging $PBF_FILES..."
 osmium merge $PBF_FILES -o data/combined.pbf
 
 echo "✅ Done: data/combined.pbf created from:"
